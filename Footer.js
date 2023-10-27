5bda55c49b772b65181b860a66cbb13b7813b8ed
yazidmufid05 <144298019+yazidmufid05@users.noreply.github.com>
2023-10-27 22:41:36 +0700

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <p>&copy; 2023 Yazid Mufid</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
